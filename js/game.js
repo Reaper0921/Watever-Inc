78f4b357ea7a21a49b391d71a68927d97f204d85
Reaper0921 <jmbrown7@email.neit.edu>
2024-11-19 13:04:33 -0500

    TEST

--- js/game.js
+++ js/game.js
@@ -14,39 +14,49 @@ var timer = setInterval(main, fps);
 
 
   //avatar but doesn't work
-  var avatar = new GameObject();
-  avatar.color = `blue`;
-  avatar.vx = 2;
-  avatar.vy = 2; 
+  // var avatar = new GameObject();
+  // avatar.color = `blue`;
+  // avatar.vx = 2;
+  // avatar.vy = 2; 
 
- 
+var enemy = new GameObject();
+enemy.color = "green";
 
 
 function main()
 {
+
+   var myGamePiece = new GameObject();
+ myGamePiece.color = `red`;
+ myGamePiece.vx = 2;
+ myGamePiece.vy = 2;
     //erases the screen
-    context.clearRect(0,0,canvas.width,canvas.height); 
+    context.clearRect(0, 0, canvas.width, canvas.height); 
 
     //moves the player if the variables in the controls.js are equal to true
-    if(d==true){ avatar.x += avatar.vx; }
-    if(a==true){ avatar.x += -avatar.vx;}
-    if(w==true){ avatar.y += -avatar.vy;}
-    if(s==true){ avatar.y += avatar.vy; }
+     if(d==true){ myGamePiece.x += myGamePiece.vx;}
+     if(a==true){ myGamePiece.x += -myGamePiece.vx;}
+     if(w==true){ myGamePiece.y += -myGamePiece.vy;}
+     if(s==true){ myGamePiece.y += myGamePiece.vy;}
 
     /*-----------collision detection--------------------
     collision detection ALWAYS goes before the render and...
     usually goes after the movement of the objects
     ---------------------------------------------------*/
-    context.drawImage(avatar, avatar.x - avatar.w / 2, avatar.y - avatar.h / 2, avatar.w, avatar.h); 
+    /*context.drawImage(myGamePiece, myGamePiece.x - myGamePiece.w / 2, myGamePiece.y - myGamePiece.h / 2, myGamePiece.w, myGamePiece.h); */
 
 
     //Keeps avatar on screen
-    if(avatar.x < 0 + avatar.w/2){avatar.x = 0 + avatar.w/2;}
-    if(avatar.x > canvas.width + -avatar.w/2){avatar.x = canvas.width + -avatar.w/2;}
-    if(avatar.y < 0 + avatar.h/2){avatar.y = 0 + avatar.h/2;}
-    if(avatar.y > canvas.height + -avatar.h/2){avatar.y = canvas.height + -avatar.h/2;}
+     if(myGamePiece.x < 0 + myGamePiece.w/2){myGamePiece.x = 0 + myGamePiece.w/2;}
+     if(myGamePiece.x > canvas.width + -myGamePiece.w/2){myGamePiece.x = canvas.width + -myGamePiece.w/2;}
+     if(myGamePiece.y < 0 + myGamePiece.h/2){myGamePiece.y = 0 + myGamePiece.h/2;}
+     if(myGamePiece.y > canvas.height + -myGamePiece.h/2){myGamePiece.y = canvas.height + -myGamePiece.h/2;}
 
-    avatar.render();
+  
+
+    myGamePiece.render();
+
+    
 }
 
 
